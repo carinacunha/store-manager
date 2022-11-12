0440dd2df5fb4658170219f466444bd148fdec06
Carina Cunha <carina.cunha13@gmail.com>
2022-11-12 20:16:53 -0300

    Requisito 2

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -3,7 +3,7 @@ const productsServices = require('../services/productsServices');
 
 const getAllProducts = async (_req, res) => {
   const products = await productsServices.getProducts();
-  return res.status(200).json(products);
+  res.status(200).json(products);
 };
 
 const getProductById = async (req, res) => {
@@ -11,15 +11,15 @@ const getProductById = async (req, res) => {
   const product = await productsServices.getById(id);
 
   if (product) {
-    return res.status(200).json(product);
+    res.status(200).json(product);
   }
-  return res.status(404).json({ message: 'Product not found' });
+  res.status(404).json({ message: 'Product not found' });
 };
 
 const insertNewProduct = async (req, res) => {
   const { name } = req.body;
   const newProduct = await productsServices.insertProduct(name);
-  return res.status(201).json(newProduct);
+  res.status(201).json(newProduct);
 };
 
 module.exports = {
---
+++ tests/unit/controllers/productsControllers.test.js
@@ -0,0 +1,69 @@
+const chai = require('chai');
+const sinon = require('sinon');
+chai.use(require('sinon-chai'));
+//const sinonChai = require('sinon-chai');
+
+//chai.use(sinonChai);
+
+const productsControllers = require('../../../src/controllers/productsController');
+const productsServices = require('../../../src/services/productsServices')
+
+describe('Products controller', function () {
+  describe('Lista todos os produtos', function() {
+    it('Listando todos os produtos', async function () {
+      const req = {};
+      const getProducts = [
+        {
+          "id": 1,
+          "name": "Martelo de Thor"
+        },
+        {
+          "id": 2,
+          "name": "Traje de encolhimento"
+        }
+      ]
+
+      const res = {};
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+
+      sinon.stub(productsServices, 'getProducts').resolves(getProducts);
+
+      await productsControllers.getAllProducts(req, res);
+
+      chai.expect(res.status).to.have.been.calledWith(200);
+      chai.expect(res.json).to.have.been.calledWith(getProducts);
+    });
+    afterEach(function () {
+      sinon.restore();
+    });
+  });
+
+  describe('Busca produto pelo Id', function () {
+    it('Buscando produto', async function () {
+      
+      const req = {
+        params: { id: 1 },
+      }
+
+      const getById = {
+          "id": 1,
+          "name": "Martelo de Thor"
+        }
+
+      const res = {};
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+
+      sinon.stub(productsServices, 'getById').resolves(getById);
+
+      await productsControllers.getProductById(req, res);
+
+      chai.expect(res.status).to.have.been.calledWith(200);
+      chai.expect(res.json).to.have.been.calledWith(getById);
+    });
+    afterEach(function () {
+      sinon.restore();
+    });
+  });
+});
