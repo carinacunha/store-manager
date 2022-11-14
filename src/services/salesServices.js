46a444d1f0a33721b60935287b4a864d89ec6f10
Carina Cunha <carina.cunha13@gmail.com>
2022-11-14 00:43:21 -0300

    Corrigi erro de linter

--- src/services/salesServices.js
+++ src/services/salesServices.js
@@ -5,7 +5,7 @@ const insertSales = async (sale) => {
   const idsProducts = sale.map(({ productId }) => productId).filter((id) => id);
   console.log(idsProducts);
 
-  const idsArray = await idsProducts.map((id) => id).map((id) => productsModels.findById(id));
+  // const idsArray = await idsProducts.map((id) => id).map((id) => productsModels.findById(id));
   
   const idExist = await productsModels.checkIds(idsProducts);
   console.log(idExist);
@@ -22,17 +22,3 @@ const insertSales = async (sale) => {
 module.exports = {
   insertSales,
 };
-
-const sale = [
-  {
-    productId: 1,
-    quantity: 1,
-  },
-  {
-    productId: 2,
-    quantity: 5,
-  },
-];
-
-const response = insertSales(sale);
-// console.log(response);
